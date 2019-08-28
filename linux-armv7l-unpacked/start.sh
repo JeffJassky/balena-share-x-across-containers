3d3b810f53f3150448c2380e5590253f9ac1609d
Jeff Jassky <jeff@jeffjassky.com>
2019-08-28 07:21:55 -0400

    Updated with latest config

--- linux-armv7l-unpacked/start.sh
+++ linux-armv7l-unpacked/start.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# By default docker gives us 64MB of shared memory size but to display heavy
-# pages we need more.
+# SHARED MEMORY
 umount /dev/shm && mount -t tmpfs shm /dev/shm
+
+# X SERVER CONFIG
+xhost +local:root
+export DISPLAY=:0
+rm -r /tmp/.X11-unix
 rm /tmp/.X0-lock &>/dev/null || true
 
-modprobe v4l2_common
+# HOST CONFIG
+echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+
+# MODPROBES
 modprobe bcm2835-v4l2
-killall /usr/lib/xorg/Xorg
-xhost +
-startx /usr/app/reflx-os --enable-logging :0
+modprobe v4l2_common
+
+# START X SERVER
+if [ ! -c /dev/fb1 ] && [ "$TFT" = "1" ]; then
+  modprobe fbtft_device name=pitft verbose=0 rotate=${TFT_ROTATE:-0} || true
+  sleep 1
+  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ') || true
+  FRAMEBUFFER=/dev/fb1 startx /usr/app/reflx-os --enable-logging
+else
+  startx /usr/app/reflx-os --enable-logging
+fi
